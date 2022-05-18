544b05d92392807b917d0df5aa5f6e17ddcfa266
Alexey Kolotilo <alexkolotilo99@gmail.com>
2022-05-18 21:35:00 +0300

    update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -13,21 +13,23 @@ public class Main {
         try(FileReader reader = new FileReader(in_str_val))
          //try(FileReader reader = new FileReader("d:/test/test.txt"))
         {
-                 // читаем посимвольно
+                      // читаем посимвольно
             int c;
             while((c=reader.read())!=-1){
 
                 System.out.print((char) c);
             }
             System.out.println(System.getProperty("line.separator") + "чтение по символам завершено" + System.getProperty("line.separator"));
+
+
         }
         catch(IOException ex){
             System.out.println("неправильное имя файла: " + ex.getMessage());
             }
 // по строкам
         try(
-             FileReader reader2 = new FileReader(in_str_val);
-               BufferedReader varRead = new BufferedReader(reader2);
+             FileReader reader = new FileReader(in_str_val);
+             BufferedReader varRead = new BufferedReader(reader);
               )
         {
             String t1=varRead.readLine();
@@ -36,10 +38,13 @@ public class Main {
                 t1=varRead.readLine();
             }
             System.out.println(System.getProperty("line.separator") + "чтение по строкам завершено");
+
         }
         catch(IOException ex){
             System.out.println("неправильное имя файла: " + ex.getMessage());
         }
+
+
         try(FileWriter writer = new FileWriter("d:/test/test_write.txt", false);
             FileReader reader2 = new FileReader("d:/test/test.txt");
             BufferedReader varRead = new BufferedReader(reader2);
@@ -52,14 +57,13 @@ public class Main {
                 t1 = varRead.readLine();
             }
 
-
         }
         catch(IOException ex){
 
             System.out.println(ex.getMessage());
         }
-     /*
+
         System.out.println(in_str_val);
-        System.out.printf("%s", in_str_val); */
+
     }
 }
